82ad7a7cb1b26a2114e6fc4ec8a479c97fac8e5e
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 01:08:09 +0530

    Update email.php

--- email.php
+++ email.php
@@ -6,15 +6,23 @@
     <?php
 //require 'sendingemail.php';
 require_once 'config.php';
-$stmt=$pdo->query("SELECT * FROM users where subscribed='yes' ");
-$emails=array();
+$stmt=$pdo->prepare("CREATE TABLE users(
+name VARCHAR(128),
+password VARCHAR(128),
+subscription VARCHAR(128)
+);
+");
+  $stmt=$pdo->exec();
+/*$emails=array();
+    
 while($row=$stmt->fetch(PDO::FETCH_ASSOC))
 {
   array_push($emails,$row['name']);
 }  
 for($i=0;$i<count($emails);$i++)
 {   echo ($emails[$i]);
-}
+}*/
+    echo "Record added';
  
 ?>
 <h1> you should receive an email </h1>
