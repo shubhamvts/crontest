40524135849347ee8791411c1cc8d9e54770386a
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 01:35:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,6 @@
 
 <?php
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+$pdo = new PDO('mysql:host=remotemysql.com;port=3306;dbname=FDjM4OJk6J', 'FDjM4OJk6J', '8jAk1cFhRs');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
