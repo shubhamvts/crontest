cf533b43dcff38c65f30bba1f194ce8c4f07b723
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-18 20:31:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title> testing cron job </title>
 </head>
   <body>
-    <h1 style="color:green"> <a href="email.php"> CLICK HERE TO RECEIVE EMAILS </a> <h1>
+    <h1 style="color:green"> <a href="email.php"> CLICK HERE TO RECEIVE EMAILS </a> </h1>
     </body>
     </html>
