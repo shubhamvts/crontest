b6709a619abce882f960ab0b610ab9b9103908b0
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-18 20:42:58 +0530

    Add files via upload

---
+++ sendingemail.php
@@ -0,0 +1,32 @@
+<?php
+require 'vendor/autoload.php';
+
+class SendEmail{
+   public static function SendMail($to,$subject,$content,$img_url){
+   $email= new \SendGrid\Mail\Mail();
+    $email->setFrom("shubhamvats830@gmail.com","Shubham Vats");
+    $email->setSubject($subject);
+    $email->addTo($to);
+    $email->addContent("text/html",$content);
+     $url=$img_url;
+
+$att1 = new \SendGrid\Mail\Attachment();
+$att1->setContent(base64_encode(file_get_contents($url)));
+$att1->setType("image/jpeg");
+$att1->setFilename("photo.");
+$att1->setDisposition("attachment");
+$email->addAttachment( $att1 );
+
+    $sendgrid = new \SendGrid(getenv('api_key'));
+
+
+    try{
+      $response = $sendgrid->send($email);
+       return $response;
+    }catch(Exception $e){
+      echo 'Caught Exception : '.$e->getMessage()."\n";
+      return false;
+    }
+  }
+}
+ ?>
