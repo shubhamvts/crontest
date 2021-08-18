b5c5fa7734c29d8137b2c06264a79bbb0f178e50
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-18 22:39:35 +0530

    Update email.php

--- email.php
+++ email.php
@@ -4,34 +4,18 @@
 </head>
   <body>
     <?php
-require 'sendingemail.php';
-require 'pdo.php';
-$stmt=$pdo->query('SELECT * FROM users where subscribed="yes" ');
-$i=0;
+//require 'sendingemail.php';
+require_once 'pdo.php';
+$stmt=$pdo->query("SELECT * FROM users where subscribed='yes' ");
 $emails=array();
 while($row=$stmt->fetch(PDO::FETCH_ASSOC))
 {
   array_push($emails,$row['name']);
-}
-
-
-    /*$url = 'https://c.xkcd.com/random/comic/';
-    $headers = @get_headers($url,1);
-    $actual_url=($headers['Location']); //redirected url from c.xkcd.com
-    $json_url = $actual_url[0].'info.0.json'; //the json of the redirected url
-    $json = file_get_contents($json_url); //getcontentoftheurl
-    $data=json_decode($json);
-    $title=$data->title;
-    $imgurl=$data->img;*/
-
+}  
 for($i=0;$i<count($emails);$i++)
 {   echo $emails[$i];
-    /*$to=$emails[$i];
-    $message = '';
-              
-              SendEmail::SendMail($to,$title,'hi',$imgurl); */
-
-  }
+}
+ 
 ?>
 <h1> you should receive an email </h1>
 </body>
