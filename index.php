9aa8b415cd3453b0eb710f16874443733a602bf0
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 15:53:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <html>
 <head>
-  <title> testing cron job </title>
+  <title> WELCOME TO 5 MINUTE COMICS </title>
+  <style>
+  form {
+    border : 2px solid green;
+    padding: 20px;
+    display: inline-block;
+
+  }
+  .cntr{
+    text-align: center;
+    position: relative;
+    top: 30%;
+    transform: translateY();
+  }
+  </style>
 </head>
   <body>
-    <h1 style="color:green"> <a href="email.php"> CLICK HERE TO RECEIVE EMAILS </a> </h1>
+    <div class="cntr">
+      <p> <h4> <span style="color:green; font-family:Arial" Welcome to 5 minute comics by XKCD </span> </h4> </p>
+    <form method="post">
+      <p> Enter your e-mail address to continue </p>
+      <p> <input style="color:whiteSmoke; background-color:black" type="submit" value="NEXT"/> </p>
+    </form>
+    </div>
     </body>
     </html>
