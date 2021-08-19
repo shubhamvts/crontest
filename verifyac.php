6d16d6ccab5309e169868c590ef75bb7397d8aba
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 19:54:52 +0530

    Update verifyac.php

--- verifyac.php
+++ verifyac.php
@@ -1,56 +1,20 @@
 <?php
-session_start();
-$pw='';
-$oldguess1=isset($_POST['vc'])?$_POST['vc']:'';
-if(! isset($_GET['name'])) die('unauthorized access');
+require_once 'config.php';
+if(! isset($_GET['name']) || ! isset($_GET['id'])) die('unauthorized access');
 require_once 'config.php';
 require 'vendor/autoload.php';
-if(!isset($_SESSION['status'])){
-    $_SESSION['status']='updated';
-$useremail=$_GET['name'];
-$pw=rand(1000,9999);
-$content ='
-<html>
-<head>
-</head>
-<body>
-<p> Your Verification Code is : <strong> '.$pw.' </strong> </p>
-<p> <strong> PLEASE NOTE THAT YOU WILL USE THIS VERIFICATION CODE FOR LOGIN. AFTER LOGGING IN, YOU CAN CHANGE YOUR PASSWORD. </strong> </p>
-</body>
-</html>
-';
-    $email= new \SendGrid\Mail\Mail();
-    $email->setFrom("shubhamvats830@gmail.com","Shubham Vats");
-    $email->setSubject("Verification Email - 5 MINUTES COMICS BY XKCD");
-    $email->addTo($useremail);
-    $email->addContent("text/html",$content);
-    $sendgrid = new \SendGrid(getenv('api_token'));
-    try{
-      $response = $sendgrid->send($email);
-       return $response;
-    }catch(Exception $e){
-      echo 'Caught Exception : '.$e->getMessage()."\n";
-      return false;
-    }
-  }
-if($oldguess1==$pw)
-{
-  $_SESSION['successful']="SUCCESSFULLY VERIFIED";
-}
-else{
-  $_SESSION['error3']="Wrong/no code entered";
-}
+$mail=$_GET['name'];
+$pw2=$_GET['id'];
+$stmt=$pdo->prepare('INSERT INTO users (name,password,subscription) VALUES (:name,:pass,:val)');
+$stmt->execute(array(
+    ':name'=>$mail,
+    ':pass'=>$pw2,
+    ':val'=> 'yes' ));
 ?>
 
 <html>
-<head>
-  <style>
-  form {
-    border : 2px solid red;
-    padding: 20px;
-    display: inline-block;
-
-  }
+    <head>
+    <style>
   .cntr{
     text-align: center;
     position: relative;
@@ -58,29 +22,11 @@ else{
     transform: translateY();
   }
   </style>
-</head>
-<body>
-  <div class="cntr">
-    <form method="post">
-    <p> <span style="font-family:Arial"> Enter the 4-digit VERIFICAION CODE sent on your email id. </span> </p>
-    <p> <input type="text" name="vc" value="<?=htmlentities($oldguess1)?>"/> </p>
-    <p> <input style="color:whiteSmoke; background-color:black" type="submit" value="CONFIRM"/> </p>
-      </form>
-    <?php
-      if(isset($_SESSION['error3'])) {
-        echo "\n <p style='color:red'>".$_SESSION['error3']."</p";
-        unset($_SESSION['error3']);
-      }
-      else if(isset($_SESSION['successful'])) {
-        echo "\n <p style='color:green'>".$_SESSION['successful']." Please proceed to <a href='login.php'> LOGIN </a> </p";
-        $stmt=$pdo->prepare('INSERT INTO users (name, password, subscription) VALUES (:name,:pw,:val)');
-        $stmt->execute(array(
-            ':name'=> $useremail,
-            ':pw'=> $pw,
-            ':val'=> 'no'));
-          unset($_SESSION['successful']);
-      }
-    ?>
-    </div>
-</body>
+    </head>
+    <body>
+        <div class="cntr">
+            <p> <h1 style="color:green"> ACCOUNT SUCCESSFULLY VERIFIED </h1> </p>
+        <p> <span style="color:red"> You are successfully subscribed to our Comics Subsciption. You will shortly start receiving emails with random comics every 5 minutes. To unsubscribe from this, click on the unsubscribe link in the emails. THANK YOU! </span> </p>
+        </div>
+    </body>
 </html>
