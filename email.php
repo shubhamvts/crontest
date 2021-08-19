e2cb133ba25ddc9faaf612167b1374a3053dfa90
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 13:57:20 +0530

    Update email.php

--- email.php
+++ email.php
@@ -6,18 +6,22 @@
     <?php
 //require 'sendingemail.php';
 require_once 'config.php';
-$stmt=$pdo->query(" SELECT # FROM users WHERE subscribed='yes' ");
-$emails=array();
+$stmt=$pdo->prepare(" INSERT INTO users (name,password,subscription) VALUES (:name,:pass,:val) ");
+    $stmt->execute(array(
+      ':name'=> 'shubhamvats830@gmail.com',
+      ':pass'=> '9operamini',
+      ':val'=> 'no' ));
+//$emails=array();
     
-while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+/*while($row=$stmt->fetch(PDO::FETCH_ASSOC))
 {
   array_push($emails,$row['name']);
 }  
 for($i=0;$i<count($emails);$i++)
 {   echo ($emails[$i]);
-}
+}*/
  
 ?>
-<h1> you should receive an email </h1>
+<h1> data inserted </h1>
 </body>
 </html>
