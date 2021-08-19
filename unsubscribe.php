22eb788fee4d117f219d4cd7f21ca54a9ff24e25
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 20:31:10 +0530

    Create unsubscribe.php

---
+++ unsubscribe.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'config.php';
+if(!isset($_GET['email']) die("Unauthorized Access");
+$unmail=$_GET['email'];
+$stmt=$pdo->prepare("UPDATE FROM users WHERE email=$unmail");
+   $stmt->execute(array(
+     ':val'=>'no';
+     ?>
+<html>
+  <head>
+    <style>
+      .cntr{
+    text-align: center;
+    position: relative;
+    top: 30%;
+    transform: translateY();
+  }
+     .cntr2{
+    border : 2px solid black;
+    padding: 20px;
+    display: inline-block;
+  }
+    </style>
+  </head>
+  <body>
+    <div class="cntr">
+      <div class="cntr2">
+        <p> You are now unsubscribed from the service. Kindly register again with your email to get 5 minute comic updates. </p>
+      </div>
+    </div>
+  </body>
+</html>
