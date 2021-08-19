684edcd7540b7589a1b862428a1b423506ac7df1
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 20:01:09 +0530

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <style>
+      .cntr{
+    text-align: center;
+    position: relative;
+    top: 30%;
+    transform: translateY();
+  }
+    </style>
+  </head>
+  <body>
+    <div class="cntr">
+      <p> <h1> Please check your email to verify yourself! Click on the verification link you received in your mail to proceed </h1> </p>
+    </div>
+  </body>
+</html>
+    
