09d3459094ab06946d6e5dbe932b54b77337bf96
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-19 20:24:57 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -7,11 +7,18 @@
     top: 30%;
     transform: translateY();
   }
+     .cntr2{
+    border : 2px solid black;
+    padding: 20px;
+    display: inline-block;
+  }
     </style>
   </head>
   <body>
     <div class="cntr">
+      <div class="cntr2">
       <p> <h1> Please check your email to verify yourself! Click on the verification link you received in your mail to proceed </h1> </p>
+    </div>
     </div>
   </body>
 </html>
